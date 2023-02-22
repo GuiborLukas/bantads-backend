b86e5e324905d3bffa0072ce52eadcaaa5d49e47
Luis Augusto Ferraz de Souza <luisprto@gmail.com>
2023-02-22 04:59:40 -0300

    Created frontend container

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,1 @@
-export const environment = {
-  production: true,
-  BASE_URL: "http://localhost:10659/",
-};
+teste
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-   BASE_URL: "http://localhost:10659/",
+  BASE_URL: "http://bantads_api_gateway:3000/",
 };
 
 /*
