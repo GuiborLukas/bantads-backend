8be0f27555ca99e4705392a2a82908d4e51ba820
Luis Augusto Ferraz de Souza <luisprto@gmail.com>
2023-02-22 23:43:36 -0300

    gerente repository

--- gerente-ws/src/main/java/br/ufpr/repository/GerenteRepository.java
+++ gerente-ws/src/main/java/br/ufpr/repository/GerenteRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.ufpr.model.Gerente;
 
-public interface GerenteRepository extends JpaRepository<Gerente, Long>{
-	
-	Optional<Gerente> findById(Long id);
-}
+public interface GerenteRepository extends JpaRepository<Gerente, Long> {
 
+	Optional<Gerente> findById(Long id);
+
+	Optional<Gerente> findByCpf(String cpf);
+
+}
